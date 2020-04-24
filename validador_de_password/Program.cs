7afb007c11114829caddf4d0c3c2db910e33ba21
IOFEL <eliofloreschoque@est.frba.utn.edu.ar>
2020-04-24 19:58:15 -0300

    inicial commit

---
+++ validador_de_password/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace validador_de_password
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var validador = new Validador();
+            
+            Console.WriteLine("Ingrese su password:");
+
+            var password = Console.ReadLine();
+
+            var resultado = validador.Validar(password) ? "valida" : "invalida";
+            
+            Console.WriteLine($"Resultado: password {resultado}");
+            
+        }
+    }
+
+
+    class Validador
+    {
+        public Boolean Validar(string password)
+        {
+            return password.Length >= 8 && password.Length <= 60;
+        }
+    }
+}
